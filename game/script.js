903f00177ab3bee7dff135f2ff245fd68addf3ca
Shalinda12345 <heshankoralagamage2002@gmail.com>
2025-11-07 23:56:24 +0530

    redesigned the game js file

--- game/script.js
+++ game/script.js
@@ -4,38 +4,56 @@ const cursor = document.querySelector('.cursor')
 const holes = [...document.querySelectorAll('.hole')]
 const scoreE1 = document.querySelector('.score span')
 let score = 0
+let hole = prepareHoles()
 
 const sound = new Audio("assets/smash.mp3")
 
 function run(){
-    const i = Math.floor(Math.random() * holes.length)
-    const hole = holes[i]
+
     let timer = null
 
-    const img = document.createElement('img')
-    img.classList.add('mole')
-    img.src = 'assets/mole.png'
+    setMole()
 
-    img.addEventListener('click', () => {
-        score += 10
-        sound.play()
-        scoreE1.textContent = score
-        img.src = 'assets/mole-whacked.png'
-        clearTimeout(timer)
-        setTimeout(() => {
-            hole.removeChild(img)
-            run()
-        }, 500)
-    })
+}
+run()
 
-    hole.appendChild(img)
+function prepareHoles(){
+    const i = Math.floor(Math.random() * holes.length)
+    const hole = holes[i]
+    return hole
+}
 
+function setMole(){
+    hole = prepareHoles()
+    const mole = document.createElement('img')
+    mole.classList.add('mole')
+    mole.src = 'assets/mole.png'
+
+    hole.appendChild(mole)
+    moleFadeTimer(mole)
+    moleClickTimer(mole)
+}
+
+function moleFadeTimer(mole){
     timer = setTimeout(() => {
-        hole.removeChild(img)
+        hole.removeChild(mole)
         run()
     }, 1500)
 }
-run()
+
+function moleClickTimer(mole){
+    mole.addEventListener('click', () => {
+        score += 10
+        sound.play()
+        scoreE1.textContent = score
+        mole.src = 'assets/mole-whacked.png'
+        clearTimeout(timer)
+        setTimeout(() => {
+            hole.removeChild(mole)
+            run()
+        }, 500)
+    })
+}
 
 window.addEventListener('mousemove', e => {
     cursor.style.top = e.pageY + 'px'
