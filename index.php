509a49d3dfff797f9d0bb63a40e31f0a86914ec8
Shalinda12345 <heshankoralagamage2002@gmail.com>
2025-11-11 21:57:23 +0530

    prepared basic UIs for Login, Registration and Home

--- index.php
+++ index.php
@@ -20,7 +20,8 @@ if (isset($_POST["leaderboard"])) {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Whack a Mole - Jungle Adventure</title>
-    <link rel="stylesheet" href="style.css" />
+    <!-- <link rel="stylesheet" href="style.css" /> -->
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
     <div class="jungle-container">
--- login/login.php
+++ login/login.php
@@ -10,7 +10,7 @@ if(isset($_SESSION["user"])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
     <div class="container">
